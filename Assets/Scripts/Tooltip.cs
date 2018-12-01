de8c526253c6fa0ab73cf0d87b17ba8433bb70fc
WouterHeslinga <Wouter_Heslinga@hotmail.com>
2018-12-01 17:49:07 +0100

    ResourceBar, Tooltips

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour
+{
+    [SerializeField] private GameObject tooltipPanel;
+    [SerializeField] private Text tooltipText;
+
+    public static Tooltip Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        //Always make sure the tooltip is last in the draw order so it's on top
+        tooltipPanel.transform.SetAsLastSibling();
+    }
+
+    private void Update()
+    {
+        tooltipPanel.transform.position = Input.mousePosition + new Vector3(tooltipPanel.GetComponent<RectTransform>().rect.width / 2, -tooltipPanel.GetComponent<RectTransform>().rect.height);
+    }
+
+    public void Hide()
+    {
+        tooltipPanel.SetActive(false);
+        tooltipText.text = "";
+    }
+
+    public void Show(string text, string title = "")
+    {
+        tooltipText.text = $"{title}\n{text}";
+        tooltipPanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/TooltipText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private string title;
+    [SerializeField] private string text;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Tooltip.Instance.Show(text, title);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Tooltip.Instance.Hide();
+    }
+}

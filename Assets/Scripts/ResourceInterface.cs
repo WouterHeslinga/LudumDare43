6fedf58ad79446d5c9dcf6c6b7d965513f519324
WouterHeslinga <Wouter_Heslinga@hotmail.com>
2018-12-01 22:33:17 +0100

    ResourceManager and interface

---
+++ Assets/Scripts/ResourceInterface.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceInterface : MonoBehaviour
+{
+    [SerializeField] private Text bones;
+    [SerializeField] private Text meat;
+    [SerializeField] private Text food;
+
+    private ResourceManager resourceManager;
+
+    private void Start()
+    {
+        resourceManager = FindObjectOfType<ResourceManager>();
+
+        resourceManager.OnResourcesChanged += UpdateInterface;
+    }
+
+    private void UpdateInterface()
+    {
+        bones.text = $"Bones {resourceManager.Resources[ResourceType.Bones]}"; 
+        meat.text = $"Meat {resourceManager.Resources[ResourceType.Meat]}"; 
+        food.text = $"Food {resourceManager.Resources[ResourceType.Food]}";
+    }
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public delegate void ResourcesChanged();
+    public ResourcesChanged OnResourcesChanged;
+
+    public Dictionary<ResourceType, int> Resources { get; private set; }
+
+    private void Start()
+    {
+        Resources = new Dictionary<ResourceType, int>()
+        {
+            {ResourceType.Bones, 5 },
+            {ResourceType.Food, 100 },
+            {ResourceType.Meat, 5 }
+        };
+        OnResourcesChanged?.Invoke();
+    }
+
+    public void AddResources(ResourceType type, int amount)
+    {
+        Resources[type] += amount;
+        OnResourcesChanged?.Invoke();
+    }
+
+    public void RemoveResources(ResourceType type, int amount)
+    {
+        if(EnoughResources(type, amount))
+            Resources[type] += amount;
+
+        OnResourcesChanged?.Invoke();
+        //TODO: error handling, show message on screen??
+    }
+
+    private bool EnoughResources(ResourceType type, int amount)
+    {
+        return Resources[type] >= amount;
+    } 
+}
